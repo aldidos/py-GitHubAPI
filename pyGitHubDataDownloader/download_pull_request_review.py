f43d90ad703c2b8c10c7bbf3517c8e8643cada49
jungil kim <jikim424@gmail.com>
2025-04-11 13:09:21 +0900

    Minor change

---
+++ pyGitHubDataDownloader/download_pull_request_review.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.append('.')
+from pyGitHubDataDownloader.executor.get_pull_request_review_executor import getPRReviewExecutor
+from pathlib import Path
+import json
+
+def download(file_path, output_dir) :
+    with open(file_path, mode = 'r') as f : 
+        dataset = json.load(f)
+
+        for data in dataset : 
+            pr_id = data['id']
+            repo_id = data['base']['repo']['id']
+            owner = data['base']['repo']['owner']['login']
+            repo = data['base']['repo']['name']
+            number = data['number']
+
+            if Path(f'{output_dir}/{pr_id}.json').exists() : 
+                continue
+
+            list_review = getPRReviewExecutor.execute(owner, repo, number)
+
+            with open(f'{output_dir}/{pr_id}.json', mode = 'w', encoding = 'utf-8') as wf : 
+                json.dump(list_review, wf)
+
+            print( f'{repo_id}/{pr_id}' )
+
+if __name__ == '__main__' : 
+    base_dir = 'e:/research_pullreq_template/PULL_REQUEST'
+    base_dir_path = Path(base_dir)
+
+    output_base_dir = './repo_content/PULL_REQUEST_REVIEW'
+
+    for dir_path in base_dir_path.iterdir() : 
+        print(dir_path.name)
+        output_dir = Path(f'{output_base_dir}/{dir_path.name}')
+        if not output_dir.exists() :             
+            output_dir.mkdir()
+
+        for file_path in dir_path.iterdir() : 
+            download(file_path, output_dir) 
---
+++ pyGitHubDataDownloader/executor/get_pull_request_review_executor.py
@@ -0,0 +1,25 @@
+from pyGitHubAPI.github_api import ghAPI
+from pyGitHubAPI.pagenator import Pagenator
+import json
+
+class GetPRReviewExecutor : 
+
+    def execute(self, owner, repo, pr_number) : 
+        res = ghAPI.get_pull_request_reviews(owner, repo, pr_number)
+
+        result = []
+
+        if res.status_code == 200 : 
+            data = json.loads(res.text)
+            result.extend(data)
+
+            pagenator = Pagenator(res)
+
+            for res in pagenator : 
+                if not res : 
+                    data = json.loads(res.text)
+                    result.extend(data) 
+
+        return result
+    
+getPRReviewExecutor = GetPRReviewExecutor()
