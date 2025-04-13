58e5dd5e4b131657efa3fc70f368bebd615992af
jungil kim <jikim424@gmail.com>
2025-04-13 10:24:23 +0900

    Modified download_pull_request_review.py

--- pyGitHubDataDownloader/download_pull_request_review.py
+++ pyGitHubDataDownloader/download_pull_request_review.py
@@ -18,24 +18,27 @@ def download(file_path, output_dir) :
             if Path(f'{output_dir}/{pr_id}.json').exists() : 
                 continue
 
-            list_review = getPRReviewExecutor.execute(owner, repo, number)
+            try : 
+                list_review = getPRReviewExecutor.execute(owner, repo, number)
 
-            with open(f'{output_dir}/{pr_id}.json', mode = 'w', encoding = 'utf-8') as wf : 
-                json.dump(list_review, wf)
+                with open(f'{output_dir}/{pr_id}.json', mode = 'w', encoding = 'utf-8') as wf : 
+                    json.dump(list_review, wf)
 
-            print( f'{repo_id}/{pr_id}' )
+                print( f'{repo_id}/{pr_id}' )
+            except : 
+                pass 
 
 if __name__ == '__main__' : 
-    base_dir = 'e:/research_pullreq_template/PULL_REQUEST'
+    base_dir = 'd:/PULL_REQUEST'
     base_dir_path = Path(base_dir)
 
     output_base_dir = './repo_content/PULL_REQUEST_REVIEW'
-
     for dir_path in base_dir_path.iterdir() : 
         print(dir_path.name)
         output_dir = Path(f'{output_base_dir}/{dir_path.name}')
+        
         if not output_dir.exists() :             
             output_dir.mkdir()
-
+            
         for file_path in dir_path.iterdir() : 
-            download(file_path, output_dir) 
+            download(file_path, output_dir)
