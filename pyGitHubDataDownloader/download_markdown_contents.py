91e1e13b8073b689d42ed051d56c629b722d271c
jungil kim <jikim424@gmail.com>
2026-03-17 15:10:13 +0900

    Modified download_markdown_contents.py and gh_content_explorer.py.

--- pyGitHubDataDownloader/download_markdown_contents.py
+++ pyGitHubDataDownloader/download_markdown_contents.py
@@ -2,45 +2,32 @@ import sys
 sys.path.append('.')
 import csv
 from pyGitHubDataDownloader.gh_content_explorer import GHContentExplorer
+import multiprocessing
+import json
 
-def explorer_pull_request_template_markdown_file(repo_owner, repo_name) : 
+def explorer_pull_request_template_markdown_file(repo_id, owner, name) : 
+    print(repo_id)
+    output_dir_path = './repo_content/REPO_MD_CONTENTS'
     contents = []
     
-    result = GHContentExplorer.find_content_with(repo_owner, repo_name, '', '.md')
+    result = GHContentExplorer.find_content_with(owner, name, '')
     contents.extend( result )
 
-    result = GHContentExplorer.find_content_with(repo_owner, repo_name, '.github', '.md')
+    result = GHContentExplorer.find_content_with(owner, name, '.github')
     contents.extend( result )
 
-    result = GHContentExplorer.find_content_with(repo_owner, repo_name, '.github/PULL_REQUEST_TEMPLATE', '.md') ####
+    result = GHContentExplorer.find_content_with(owner, name, '.github/PULL_REQUEST_TEMPLATE') ####
     contents.extend( result )
-
-    return contents
-
-output_file = open('./repo_markdown_contents.txt', mode = 'w', encoding = 'utf-8')
+    
+    with open(f'{output_dir_path}/{repo_id}.json', mode = 'w', encoding = 'utf-8') as f : 
+        json.dump(contents, f)
 
 if __name__ == '__main__' : 
-
-    input_file_path = './temp.csv'
-
+    input_file_path = 'e:/research_pullreq_template_checklist/data/download_markdown_content.csv'
+    
     with open(input_file_path, mode = 'r', encoding = 'utf-8-sig') as f : 
         reader = csv.DictReader(f)
+        repo_list = [ ( row['repo_id'], row['owner'], row['name'] ) for row in reader  ]        
         
-        for row in reader :             
-            repo_id = row['repo_id']
-            repo_owner = row['repo_owner']
-            repo_name = row['repo_name']
-
-        contents = explorer_pull_request_template_markdown_file(repo_owner, repo_name)
-
-        for content in contents : 
-            name = content['name']
-            path = content['path']
-            download_url = content['download_url']
-
-            output_str = f'{repo_id}\t{repo_owner}\t{repo_name}\t{name}\t{path}\t{download_url}\n'
-            output_file.write( output_str )
-
-        print(repo_id)
-    
-    output_file.close()
+        with multiprocessing.Pool() as pool : 
+            pool.starmap( explorer_pull_request_template_markdown_file,  repo_list )
--- pyGitHubDataDownloader/gh_content_explorer.py
+++ pyGitHubDataDownloader/gh_content_explorer.py
@@ -1,27 +1,18 @@
-from pyGitHubAPI.github_api import ghAPI, delay
+from pyGitHubAPI.github_api import ghAPI
+from pyGitHubAPI.util import delay
 import json
-import re
-import time
-
-def is_pull_request_template(file_name : str) : 
-    str = file_name.lower()
-    mat = re.match('pull_request_template.md', str)
-    if mat : 
-        return True
-    return False
 
 class GHContentExplorer : 
 
-    def find_content_with(repo_owner, repo_name, path, file_extension) : 
+    def find_content_with(repo_owner, repo_name, path) : 
         contents = []
+
+        res = ghAPI.get_rate_limit()
+        delay(res)
+
         res = ghAPI.get_repository_content(repo_owner, repo_name, path)
         if res.status_code == 200 : 
             temp_contents = json.loads( res.text ) 
-
-            for content in temp_contents : 
-                if content['name'].endswith(file_extension) : 
-                    contents.append( content )
-        
-        delay(res)
+            contents.extend( temp_contents )
+       
         return contents
-    
